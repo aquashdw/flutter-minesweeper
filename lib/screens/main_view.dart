7a40c3cfd47a80e8abb50a1dd01ebcf5f09af19c
aquashdw <aquashdw@gmail.com>
2023-03-23 23:52:05 +0900

    main screen

--- lib/screens/main_view.dart
+++ lib/screens/main_view.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:minesweeper/screens/game_view.dart';
 
@@ -8,21 +10,23 @@ class MainView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightBlue,
-      appBar: AppBar(
-        backgroundColor: Colors.blueAccent,
-        title: const Text("MineSweeper"),
-      ),
       body: SafeArea(
         child: LayoutBuilder(builder: (context, constraints) {
           var height = constraints.maxHeight;
           var width = constraints.maxWidth;
           // stack UI horizontally
-          if (width * 5 > height * 8) {
-            return _HorizontalLayout(width: width);
+          if (width * 5 >= height * 8) {
+            return _HorizontalLayout(
+              width: width,
+              height: height,
+            );
           }
           // stack UI vertically
           else {
-            return _VerticalLayout(width: width);
+            return _VerticalLayout(
+              width: width,
+              height: height,
+            );
           }
         }),
       ),
@@ -33,18 +37,48 @@ class MainView extends StatelessWidget {
 class _VerticalLayout extends StatelessWidget {
   const _VerticalLayout({
     required this.width,
+    required this.height,
   });
 
   final double width;
+  final double height;
 
   @override
   Widget build(BuildContext context) {
+    var mineSize = width < height ? width * 0.4 : height * 0.4;
+    var fontSize = (height - mineSize) * 0.09;
     return Column(
       children: [
         Flexible(
           flex: 5,
           child: Container(
-            decoration: const BoxDecoration(color: Colors.blue),
+            decoration: const BoxDecoration(color: Colors.lightBlue),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "aquashdw's",
+                    style: TextStyle(
+                      fontSize: fontSize * 0.7,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  _MineIcon(mineSize: mineSize),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "MineSweeper",
+                    style: TextStyle(
+                      fontSize: fontSize,
+                    ),
+                  )
+                ],
+              ),
+            ),
           ),
         ),
         Flexible(
@@ -58,19 +92,19 @@ class _VerticalLayout extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: const [
                 _NewGameButton(
-                  difficulty: "Easy (8 * 16, 10)",
+                  difficulty: "Easy",
                   sizeX: 8,
                   sizeY: 16,
                   mineCount: 10,
                 ),
                 _NewGameButton(
-                  difficulty: "Medium (10 * 18, 35)",
+                  difficulty: "Medium",
                   sizeX: 10,
                   sizeY: 18,
                   mineCount: 35,
                 ),
                 _NewGameButton(
-                  difficulty: "Hard (14 * 26, 75)",
+                  difficulty: "Hard",
                   sizeX: 14,
                   sizeY: 26,
                   mineCount: 75,
@@ -87,18 +121,48 @@ class _VerticalLayout extends StatelessWidget {
 class _HorizontalLayout extends StatelessWidget {
   const _HorizontalLayout({
     required this.width,
+    required this.height,
   });
 
   final double width;
+  final double height;
 
   @override
   Widget build(BuildContext context) {
+    var mineSize = width < height ? width * 0.65 : height * 0.65;
+    var fontSize = height * 0.07;
     return Row(
       children: [
         Flexible(
           flex: 5,
           child: Container(
-            decoration: const BoxDecoration(color: Colors.blue),
+            decoration: const BoxDecoration(color: Colors.lightBlue),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "aquashdw's",
+                    style: TextStyle(
+                      fontSize: fontSize * 0.7,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  _MineIcon(mineSize: mineSize),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "MineSweeper",
+                    style: TextStyle(
+                      fontSize: fontSize,
+                    ),
+                  )
+                ],
+              ),
+            ),
           ),
         ),
         Flexible(
@@ -113,19 +177,19 @@ class _HorizontalLayout extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: const [
                 _NewGameButton(
-                  difficulty: "Easy (9 * 9, 10)",
+                  difficulty: "Easy",
                   sizeX: 9,
                   sizeY: 9,
                   mineCount: 10,
                 ),
                 _NewGameButton(
-                  difficulty: "Medium (16 * 16, 40)",
+                  difficulty: "Medium",
                   sizeX: 16,
                   sizeY: 16,
                   mineCount: 40,
                 ),
                 _NewGameButton(
-                  difficulty: "Hard (30 * 16, 99)",
+                  difficulty: "Hard",
                   sizeX: 30,
                   sizeY: 16,
                   mineCount: 99,
@@ -179,6 +243,98 @@ class _NewGameButton extends StatelessWidget {
             color: Colors.blue,
           ),
         ),
+      ),
+    );
+  }
+}
+
+class _MineIcon extends StatelessWidget {
+  const _MineIcon({
+    required this.mineSize,
+  });
+
+  final double mineSize;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: mineSize,
+      height: mineSize,
+      child: Stack(
+        children: [
+          Center(
+            child: Container(
+              width: mineSize * 0.8,
+              height: mineSize * 0.8,
+              decoration: BoxDecoration(
+                color: Colors.blueAccent[700],
+                borderRadius: BorderRadius.circular(mineSize * 0.4),
+              ),
+            ),
+          ),
+          Center(
+            child: Container(
+              width: mineSize,
+              height: mineSize * 0.1,
+              decoration: BoxDecoration(
+                color: Colors.blueAccent[700],
+                borderRadius: BorderRadius.circular(mineSize * 0.05),
+              ),
+            ),
+          ),
+          Center(
+            child: Transform.rotate(
+              angle: pi * 0.25,
+              child: Container(
+                width: mineSize,
+                height: mineSize * 0.1,
+                decoration: BoxDecoration(
+                  color: Colors.blueAccent[700],
+                  borderRadius: BorderRadius.circular(mineSize * 0.05),
+                ),
+              ),
+            ),
+          ),
+          Center(
+            child: Transform.rotate(
+              angle: -pi * 0.25,
+              child: Container(
+                width: mineSize,
+                height: mineSize * 0.1,
+                decoration: BoxDecoration(
+                  color: Colors.blueAccent[700],
+                  borderRadius: BorderRadius.circular(mineSize * 0.05),
+                ),
+              ),
+            ),
+          ),
+          Center(
+            child: Transform.rotate(
+              angle: pi * 0.5,
+              child: Container(
+                width: mineSize,
+                height: mineSize * 0.1,
+                decoration: BoxDecoration(
+                  color: Colors.blueAccent[700],
+                  borderRadius: BorderRadius.circular(mineSize * 0.05),
+                ),
+              ),
+            ),
+          ),
+          Center(
+            child: Transform.translate(
+              offset: Offset(-mineSize * 0.1, -mineSize * 0.1),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.lightBlue,
+                  borderRadius: BorderRadius.circular(mineSize * 0.1),
+                ),
+                width: mineSize * 0.2,
+                height: mineSize * 0.2,
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
