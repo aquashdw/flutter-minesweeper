65337f1cab5faf38fc0bb4b3d9965c0a478074bf
aquashdw <aquashdw@gmail.com>
2023-03-23 22:59:07 +0900

    divide difficulties based on layout

--- lib/screens/main_view.dart
+++ lib/screens/main_view.dart
@@ -18,97 +18,123 @@ class MainView extends StatelessWidget {
           var width = constraints.maxWidth;
           // stack UI horizontally
           if (width * 5 > height * 8) {
-            return Row(
-              children: [
-                Flexible(
-                  flex: 5,
-                  child: Container(
-                    decoration: const BoxDecoration(color: Colors.blue),
-                  ),
-                ),
-                Flexible(
-                  flex: 3,
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(
-                      horizontal: width * 0.025,
-                      // vertical: height * 0.3,
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: const [
-                        _NewGameButton(
-                          difficulty: "Easy",
-                          sizeX: 8,
-                          sizeY: 16,
-                          mineCount: 10,
-                        ),
-                        _NewGameButton(
-                          difficulty: "Medium",
-                          sizeX: 10,
-                          sizeY: 18,
-                          mineCount: 35,
-                        ),
-                        _NewGameButton(
-                          difficulty: "Hard",
-                          sizeX: 14,
-                          sizeY: 26,
-                          mineCount: 75,
-                        ),
-                      ],
-                    ),
-                  ),
-                )
-              ],
-            );
+            return _HorizontalLayout(width: width);
           }
           // stack UI vertically
           else {
-            return Column(
-              children: [
-                Flexible(
-                  flex: 5,
-                  child: Container(
-                    decoration: const BoxDecoration(color: Colors.blue),
-                  ),
-                ),
-                Flexible(
-                  flex: 3,
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(
-                      horizontal: width * 0.15,
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: const [
-                        _NewGameButton(
-                          difficulty: "Easy",
-                          sizeX: 8,
-                          sizeY: 16,
-                          mineCount: 10,
-                        ),
-                        _NewGameButton(
-                          difficulty: "Medium",
-                          sizeX: 10,
-                          sizeY: 18,
-                          mineCount: 35,
-                        ),
-                        _NewGameButton(
-                          difficulty: "Hard",
-                          sizeX: 14,
-                          sizeY: 26,
-                          mineCount: 75,
-                        ),
-                      ],
-                    ),
-                  ),
-                )
-              ],
-            );
+            return _VerticalLayout(width: width);
           }
         }),
       ),
+    );
+  }
+}
+
+class _VerticalLayout extends StatelessWidget {
+  const _VerticalLayout({
+    required this.width,
+  });
+
+  final double width;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Flexible(
+          flex: 5,
+          child: Container(
+            decoration: const BoxDecoration(color: Colors.blue),
+          ),
+        ),
+        Flexible(
+          flex: 3,
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: width * 0.15,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: const [
+                _NewGameButton(
+                  difficulty: "Easy (8 * 16, 10)",
+                  sizeX: 8,
+                  sizeY: 16,
+                  mineCount: 10,
+                ),
+                _NewGameButton(
+                  difficulty: "Medium (10 * 18, 35)",
+                  sizeX: 10,
+                  sizeY: 18,
+                  mineCount: 35,
+                ),
+                _NewGameButton(
+                  difficulty: "Hard (14 * 26, 75)",
+                  sizeX: 14,
+                  sizeY: 26,
+                  mineCount: 75,
+                ),
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class _HorizontalLayout extends StatelessWidget {
+  const _HorizontalLayout({
+    required this.width,
+  });
+
+  final double width;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Flexible(
+          flex: 5,
+          child: Container(
+            decoration: const BoxDecoration(color: Colors.blue),
+          ),
+        ),
+        Flexible(
+          flex: 3,
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: width * 0.025,
+              // vertical: height * 0.3,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: const [
+                _NewGameButton(
+                  difficulty: "Easy (9 * 9, 10)",
+                  sizeX: 9,
+                  sizeY: 9,
+                  mineCount: 10,
+                ),
+                _NewGameButton(
+                  difficulty: "Medium (16 * 16, 40)",
+                  sizeX: 16,
+                  sizeY: 16,
+                  mineCount: 40,
+                ),
+                _NewGameButton(
+                  difficulty: "Hard (30 * 16, 99)",
+                  sizeX: 30,
+                  sizeY: 16,
+                  mineCount: 99,
+                ),
+              ],
+            ),
+          ),
+        )
+      ],
     );
   }
 }
